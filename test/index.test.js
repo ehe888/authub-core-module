2f08eaa44e11beb0490d2dd1ea192fe8f9ada7dd
Eric He <eric.leihe@gmail.com>
2016-04-08 10:42:06 +0800

    administrator account activation with vericode filter

--- test/index.test.js
+++ test/index.test.js
@@ -23,7 +23,7 @@ let accountName = "test_account";
 let clientId = "";
 let clientSecret = "";
 
-describe("Identity", function(){
+describe("OAuth2 Identity Service", function(){
   before(function(done){
     console.log("before all tests - clean up master db");
 
@@ -56,7 +56,7 @@ describe("Identity", function(){
 
   let jwtToken;
 
-  it("should create a account", function(done){
+  it("should create an account and its owner administrator", function(done){
     request(app)
       .post('/identity/register')
       .send({
@@ -81,7 +81,7 @@ describe("Identity", function(){
       });
   });
 
-  it("should failed when account name is invalid", function(done){
+  it("should get 500 error if account name is invalid", function(done){
     request(app)
       .get('/identity')
       .set('X-Authub-Account', 'invalid_account')
@@ -94,33 +94,103 @@ describe("Identity", function(){
       });
   });
 
-  it("should success to create administrator when account name is valid", function(done){
+  it("should failed to get administrator token using password granty type when user is not activated!", function(done){
     var data = {
-      username: 'ehe888',
-      password: '123456',
-      mobile: '13764211365',
-      email: 'lei.he@aivics.com',
-      firstName: 'Lei',
-      lastName: 'He'
+      username: 'admin',
+      password: 'abc123456',
+      grant_type: 'password',
+      user_type: 'admin'
     }
     request(app)
-      .post('/identity/administrators')
+      .post('/identity/oauth2/token')
       .set('X-Authub-Account', accountName)
       .send(data)
-      .expect(201)
+      .expect(403)
       .end(function(err, res){
         if (err) {
           return done(err);
         }
+
+        console.log(res.body);
+
+        //jwtToken = res.body;
+
+        done();
+      });
+  });
+
+  it("should failed to activate administrator without vericode_token", function(done){
+    request(app)
+      .get('/identity/administrators/activate')
+      .set('X-Authub-Account', accountName)
+      .expect(403)
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        console.log(res.body);
+        done();
+      });
+  });
+
+  process.env.NODE_ENV = 'local-unit-test';
+  var code;
+  var vericode_token;
+  var data = { identity: 'lei.he@fastccm.com' , veri_type: 'email', target_url: "/administrators/activate" };
+
+  it("should success to get veri code", function(done){
+    request(app)
+      .post('/identity/vericode/code')
+      .send( data )
+      .expect(201)
+      .expect(function(res){
+
+        code = res.body.veri_code;
+        console.log("code: ", code);
+
+        if( res.body.success !== true ) throw new Error('invalid_response');
+        if( process.env.NODE_ENV === 'local-unit-test' && !res.body.veri_code ) throw new Error('invalid_code_returned');
+      })
+      .end(done);
+  });
+
+  it('should return token', function(done){
+
+    console.log(identity + " & " + code);
+
+    var data = { identity: 'lei.he@fastccm.com', veri_code: code };
+    request(app)
+      .post('/identity/vericode/validate')
+      .send( data )
+      .expect(201)
+      .end(function(err, res){
+        if (err) return done(err);
+        vericode_token = res.body.vericode_token;
+        console.log(vericode_token);
+        done();
+      });
+  });
+
+  it("should success to activate administrator with vericode_token", function(done){
+    request(app)
+      .get('/identity/administrators/activate')
+      .set('X-Authub-Account', accountName)
+      .send({ vericode_token: vericode_token })
+      .expect(200, { success: true })
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        console.log(res.body);
         done();
       });
   });
 
 
-  it("should success to get admin token using password granty type", function(done){
+  it("should success to get administrator token using password grant type when user is activated!", function(done){
     var data = {
-      username: 'ehe888',
-      password: '123456',
+      username: 'admin',
+      password: 'abc123456',
       grant_type: 'password',
       user_type: 'admin'
     }
@@ -141,6 +211,78 @@ describe("Identity", function(){
         done();
       });
   });
+
+
+  it("should success to create administrator when account name is valid", function(done){
+    var data = {
+      username: 'ehe888',
+      password: '123456',
+      mobile: '13764211365',
+      email: 'lei.he@aivics.com',
+      firstName: 'Lei',
+      lastName: 'He'
+    }
+    request(app)
+      .post('/identity/administrators')
+      .set('X-Authub-Account', accountName)
+      .set("Authorization", "Bearer " + jwtToken.access_token )
+      .send(data)
+      .expect(201)
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+
+  it("should failed to create administrator when username is duplicated", function(done){
+    var data = {
+      username: 'ehe888',
+      password: '123456',
+      mobile: '13764211365',
+      email: 'lei.he@aivics.com',
+      firstName: 'Lei',
+      lastName: 'He'
+    }
+    request(app)
+      .post('/identity/administrators')
+      .set('X-Authub-Account', accountName)
+      .set("Authorization", "Bearer " + jwtToken.access_token )
+      .send(data)
+      .expect(500)
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+
+  it("should failed to create administrator when email is duplicated", function(done){
+    var data = {
+      username: 'ehe8888',
+      password: '123456',
+      mobile: '137642113658',
+      email: 'lei.he@aivics.com',
+      firstName: 'Lei',
+      lastName: 'He'
+    }
+    request(app)
+      .post('/identity/administrators')
+      .set('X-Authub-Account', accountName)
+      .set("Authorization", "Bearer " + jwtToken.access_token )
+      .send(data)
+      .expect(500)
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+
+
 
 
   it("should success to create client when account name is valid", function(done){
@@ -200,7 +342,6 @@ describe("Identity", function(){
     request(app)
       .post('/identity/oauth2/token')
       .set('X-Authub-Account', accountName)
-      .set("Authorization", "Bearer " + jwtToken.access_token )
       .send(data)
       .expect(200)
       .end(function(err, res){
